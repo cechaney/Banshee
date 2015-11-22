973b04398a05409567006465885bb5338bb83176
Christopher Chaney <chaneymail@gmail.com>
2015-11-22 00:42:11 -0600

    Oh my!  Lots of erorr handling

--- banshee.js
+++ banshee.js
@@ -15,9 +15,9 @@ var config = require('./config.json');
 
 	var onRequest = function(req, res){
 
-		console.log('Proxy request:' + req.url);
-
 		try{
+
+			console.log('Proxy request:' + req.url);			
 
 			if(req.url.indexOf('favicon') > 0){
 	  			res.setStatus = 404;
@@ -51,39 +51,61 @@ var config = require('./config.json');
 
 	  		var proxy = http.request(options, function (resp) {
 
-	    		resp.pipe(res, {
-	      			end: true
-	    		});
+	  			try{
+
+		    		resp.pipe(res, {
+		      			end: true
+		    		});	  				
+
+	  			} catch(error){
+	  				console.log('Error on proxy request');
+	  			}
 
 	  		});
 
 	  		proxy.setTimeout(config.timeout, function(){
 
-	  			console.log('Proxy timeout');
+	  			try{
 
-	  			res.setStatus = 504;
-	  			res.end();
+		  			console.log('Proxy timeout');
+
+		  			res.setStatus = 504;
+		  			res.end();	  				
+
+	  			} catch(error){
+	  				console.log('Error on handle of proxy timeout');
+	  			}
 
 	  		});
 
 	  		proxy.on('error', function(error){
 
-	  			console.log('Proxy error: ' + error.message);
+	  			try{
 
-	  			res.setStatus = 500;
-	  			res.end();
+		  			console.log('Proxy error: ' + error.message);
 
-	  			return;
+		  			res.setStatus = 500;
+		  			res.end();	  				
+
+	  			} catch(error){
+	  				console.log('Error on proxy error handle');
+	  			}
 
 	  		});
 
 	  		res.on('finish', function(){
 
-	  			if(workerIndex){
-	  				workerPool.free.push(workerIndex);
+	  			try{
+
+		  			if(workerIndex){
+		  				workerPool.free.push(workerIndex);
+		  			}
+
+	  			} catch(error){
+					console.log('Error on request finish');
 	  			}
 
-	  		})
+	  		});
 
   			if(worker){
 		  		req.pipe(proxy, {
@@ -105,58 +127,79 @@ var config = require('./config.json');
 
 	var startProxyEndpoint = function(){
 
-		http.createServer(function (req, res) {
-			onRequest(req, res);
-		}).listen(config.port, config.host, function(){
-			console.log('WebRender Proxy Server running at http://' + config.host + ':' + config.port + '/');
-		});
+		try{
+
+			http.createServer(function (req, res) {
+				onRequest(req, res);
+			}).listen(config.port, config.host, function(){
+				console.log('WebRender Proxy Server running at http://' + config.host + ':' + config.port + '/');
+			});
+
+		} catch(error){
+			console.log('Error on proxy endpoint start');
+		}
+
 	}
 
 	var startPool = function(){
 
-		var workerPort = config.workerConfig.startingPort;
+		try{
 
-		for(i = 0; i < config.workerCount; i++){
+			var workerPort = config.workerConfig.startingPort;
 
-			worker = {
-				port: workerPort,
-				process: null
-			}
+			for(i = 0; i < config.workerCount; i++){
 
-			worker.process = respawn(
-				[
-					'phantomjs',
-					'--disk-cache=no',
-					//'--load-images=true', //Do not enable this.  https://github.com/ariya/phantomjs/issues/12903
-					'--ignore-ssl-errors=yes',
-					'--ssl-protocol=any',
-					'worker.js',
-					'--port=' + workerPort
-				],
-				{
-	        		cwd: '.',
-	        		maxRestarts:6,
-	        		sleep: 5000,
-	        		kill: 1000,
-	        		stdio: [0, 1, 2]
-      			}
-      		);
+				worker = {
+					port: workerPort,
+					process: null
+				}
 
-      		workerPool.workers[i] = worker;
+				worker.process = respawn(
+					[
+						'phantomjs',
+						'--disk-cache=no',
+						//'--load-images=true', //Do not enable this.  https://github.com/ariya/phantomjs/issues/12903
+						'--ignore-ssl-errors=yes',
+						'--ssl-protocol=any',
+						'worker.js',
+						'--port=' + workerPort
+					],
+					{
+		        		cwd: '.',
+		        		maxRestarts:6,
+		        		sleep: 5000,
+		        		kill: 1000,
+		        		stdio: [0, 1, 2]
+	      			}
+	      		);
 
-      		workerPool.workers[i].process.start();
+	      		workerPool.workers[i] = worker;
 
-      		workerPool.free.push(i);
+	      		workerPool.workers[i].process.start();
 
-      		workerPort++;
+	      		workerPool.free.push(i);
 
+	      		workerPort++;
+
+			}			
+
+		} catch(error){
+			console.log('Error on PhantomJS pool start');
 		}
 
 	}
 
 	var boot = function(){
-		startPool();
-		startProxyEndpoint();
+
+		try{
+
+			startPool();
+			startProxyEndpoint();
+
+		} catch(error){
+			console.log('Error on Banshee boot');
+		}
+
 	}
 
 	boot();
