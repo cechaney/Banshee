470cc0c953ca18d7603e1f9ceab2e1a4459137ed
chrischaney <christopher_chaney@homedepot.com>
2015-12-04 10:47:34 -0600

    Chanages to wait for free PhantomJS instance

--- banshee.js
+++ banshee.js
@@ -28,22 +28,65 @@ var config = require('./config.json');
 		try{
 
 			if (req.url === '/favicon.ico') {
+				
 				res.writeHead(200, {'Content-Type': 'image/x-icon'} );
 				res.end();
+				
 				return;
+
 			}
 
 			logger.debug('Proxy request:' + req.url);			
 
 			var workerIndex = null;
 
-			if(workerPool && workerPool.workers.length > 0 && workerPool.free.length > 0){
-				workerIndex = workerPool.free.pop();
-			} else {
-				res.setStatus(429);
-				res.end();
-				return;
-			}
+			var poolCheckCount = 0;
+
+			var poolCheckInterval = setInterval(function(){
+
+				if((poolCheckCount * config.workerPoolCheckInterval) >= config.requestTimeout){
+					
+					clearInterval(poolCheckInterval);
+
+					res.statusCode = 429;
+					res.end();
+					
+					return;
+
+				} else {
+
+					poolCheckCount++;
+
+					if(workerPool && workerPool.workers.length > 0 && workerPool.free.length > 0){
+
+						clearInterval(poolCheckInterval);
+
+						workerIndex = workerPool.free.pop();
+
+						proxyRequest(workerIndex, req, res)
+
+					}
+
+				}
+
+			}, config.workerPoolCheckInterval);
+
+
+  		} catch(error){
+
+  			logger.error('Request error: ' + error.message);
+
+  			res.statusCode = 500;
+  			res.end;  			
+
+  			return;
+  		}
+
+	};
+
+	var proxyRequest = function(workerIndex, req, res){
+
+		try{
 
 			var reqMethod = req.method;
 			var workerPort = workerPool.workers[workerIndex].port;
@@ -73,15 +116,12 @@ var config = require('./config.json');
 
 	  		});
 
-	  		proxy.setTimeout(config.timeout, function(){
+	  		proxy.setTimeout(config.proxyLoadTimeout, function(){
 
 	  			try{
-
-		  			logger.error('Proxy timeout');
-
-		  			res.setStatus = 504;
-		  			res.end();	  				
-
+		  			logger.error('Proxy load timeout');
+		  			res.statusCode = 504;
+		  			res.end();
 	  			} catch(error){
 	  				logger.error('Error on handle of proxy timeout: ' + error.message);
 	  			}
@@ -91,12 +131,9 @@ var config = require('./config.json');
 	  		proxy.on('error', function(error){
 
 	  			try{
-
 		  			logger.error('Proxy error: ' + error.message);
-
-		  			res.setStatus = 500;
-		  			res.end();	  				
-
+		  			res.statusCode = 500;
+		  			res.end();
 	  			} catch(error){
 	  				logger.error('Error on proxy error handle: ' + error.message);
 	  			}
@@ -107,7 +144,7 @@ var config = require('./config.json');
 
 	  			try{
 
-		  			if(workerIndex){
+		  			if(workerIndex !== undefined  && workerIndex !== null){
 		  				workerPool.free.push(workerIndex);
 		  			}
 
@@ -117,23 +154,28 @@ var config = require('./config.json');
 
 	  		});
 
-  			if(worker){
-		  		req.pipe(proxy, {
-		    		end: true
-		  		});
-  			} else {
-				res.setStatus(429);
-				res.end();
-  			}
+				if(worker){
+
+			  		req.pipe(proxy, {
+			    		end: true
+			  		});
+
+				} else {
+					res.statusCode = 429;
+					res.end();
+				}
 
   		} catch(error){
-  			logger.error('Request error: ' + error.message);
-  			res.setStatus = 500;
-  			res.end();
-  			return;
-  		}
 
-	};
+  			logger.error('Proxy request error: ' + error.message);
+
+  			res.statusCode = 500;
+  			res.end;  			
+
+  			return;
+  		}		
+
+	}
 
 	var startProxyEndpoint = function(){
 
@@ -143,6 +185,7 @@ var config = require('./config.json');
 				onRequest(req, res);
 			}).listen(config.port, config.host, function(){
 				logger.info('WebRender Proxy Server running at http://' + config.host + ':' + config.port + '/');
+				logger.info('Proxying traffic for: ' + config.targetHost);
 			});
 
 		} catch(error){
